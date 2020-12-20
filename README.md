332f46c5068d04cfd8d05d157e1d174723162ce4
stever410 <ngotanduc410@gmail.com>
2020-12-20 17:55:41 +0700

    Update ui

---
+++ README.md
@@ -0,0 +1,2 @@
+# ECOMMERCE WEBSITE
+Design: https://dribbble.com/shots/9517380-Languish-Minimal-Designer-Category-Page-Exploration-E-com/attachments/1543371?mode=media
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,7 @@ module.exports = {
   theme: {
     extend: {
       fontFamily: {
-        custom: ["Montserrat", "sans-serif"],
+        custom: ["Quicksand", "sans-serif"],
       },
     },
   },
